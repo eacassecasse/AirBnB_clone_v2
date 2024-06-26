0ce751bfa544b912e6c04dfd9392e5b079911ac7
Edmilson de Azevedo Cassecasse <55765693+eacassecasse@users.noreply.github.com>
2024-06-26 20:03:22 +0200

    fix: change the list from storage.all() to use .to_dict() method

--- console.py
+++ console.py
@@ -233,10 +233,10 @@ class HBNBCommand(cmd.Cmd):
                 print("** class doesn't exist **")
                 return
             for k, v in storage.all(HBNBCommand.classes[args]).items():
-                print_list.append(str(v))
+                print_list.append(str(v.to_dict()))
         else:
             for k, v in storage.all().items():
-                print_list.append(str(v))
+                print_list.append(str(v.to_dict()))
 
         print(print_list)
 
