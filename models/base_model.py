48480fc98b4db039a0156bbfc412a3d3a1eaeff3
Edmilson de Azevedo Cassecasse <55765693+eacassecasse@users.noreply.github.com>
2024-06-29 19:22:56 +0200

    refactor: Updating the BaseModel to assure it removes the __class__ key from the dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -15,31 +15,24 @@ class BaseModel:
     created_at = Column(
         DateTime,
         nullable=False,
-        default=datetime.utcnow(),
-        server_default=text('CURRENT_TIMESTAMP'))
+        default=datetime.utcnow())
     updated_at = Column(
         DateTime,
         nullable=False,
-        default=datetime.utcnow(),
-        server_default=text('CURRENT_TIMESTAMP'))
+        default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        self.id = str(uuid.uuid4())
         if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            created = kwargs.get('created_at', '')
-            updated = kwargs.get('updated_at', '')
-
-            kwargs['updated_at'] = datetime.strptime(
-                updated, '%Y-%m-%dT%H:%M:%S.%f') if updated else datetime.now()
-            kwargs['created_at'] = datetime.strptime(
-                created, '%Y-%m-%dT%H:%M:%S.%f') if updated else datetime.now()
-            kwargs.pop('__class__', ' ')
-            self.__dict__.update(kwargs)
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    val = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != "__class__":
+                    setattr(self, key, val)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -66,7 +59,7 @@ class BaseModel:
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         if '_sa_instance_state' in dictionary:
-            dictionary.pop('_sa_instance_state', '')
+            dictionary.pop('_sa_instance_state', None)
         return dictionary
 
     def delete(self):
