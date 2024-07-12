ff574d1c40ec315e513322a71eac55318d955464
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2024-07-12 04:20:52 -0700

    fix: Importing functions put and run in order to upload content to the server and unpackaging it by running a command remotely

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ Fabfile to generate a .tgz archive from the contents of web_static directory.
 
 import os
 from datetime import datetime
-from fabric.api import env, local
+from fabric.api import env, local, put, run
 
 # Hosts IP and user of the web server web-01 and web-02
 env.hosts = ["54.157.136.194", "100.25.134.41"]
