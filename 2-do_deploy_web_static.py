fc52b5ba8497be0b6391f6454c16e19b8231b3a6
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2024-07-12 06:39:47 -0700

    fix: Still trying to solve bugs

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -48,24 +48,25 @@ def do_deploy(archive_path):
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
-    if not os.path.isfile(archive_path):
+    if not path.exists(archive_path):
         return False
-
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
-
     try:
-        path = "/data/web_static/releases/"
-        put(archive_path, "/tmp/{}".format(file))
-        run("rm -rf {}{}/".format(path, name))
-        run("mkdir -p {}{}/".format(path, name))
-        run("tar -xzf /tmp/{} -C {}{}/".format(file, path, name))
-        run("rm /tmp/{}".format(file))
-        run("mv {}{}/web_static/* {}{}/".format(path, name, path, name))
-        run("rm -rf {}{}/web_static".format(path, name))
+        tgzfile = archive_path.split("/")[-1]
+        print(tgzfile)
+        filename = tgzfile.split(".")[0]
+        print(filename)
+        pathname = "/data/web_static/releases/" + filename
+        put(archive_path, '/tmp/')
+        run("mkdir -p /data/web_static/releases/{}/".format(filename))
+        run("tar -zxvf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(tgzfile, filename))
+        run("rm /tmp/{}".format(tgzfile))
+        run("mv /data/web_static/releases/{}/web_static/*\
+            /data/web_static/releases/{}/".format(filename, filename))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
         run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(path, name))
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(filename))
         return True
     except Exception as e:
-        print(f"Deployment failed: {e}")
         return False
