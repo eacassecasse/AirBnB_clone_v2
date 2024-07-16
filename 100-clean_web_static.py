6bf477da489b9272f67a1490fff3b925e71f5489
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2024-07-16 03:35:29 -0700

    fix: Correcting import issues

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -5,7 +5,7 @@ Fabfile to generate a .tgz archive from the contents of web_static directory.
 
 import os
 from datetime import datetime
-from fabric.api import local
+from fabric.api import env, local, sudo
 
 # Hosts IP and user of the web server web-01 and web-02
 env.hosts = ["54.157.136.194", "100.25.134.41"]
@@ -28,6 +28,15 @@ def do_clean(number=0):
     else:
         number += 1
 
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    # Local cleanup
+    print(f"Cleaning local archives, keeping {number-1} most recent.")
+    local('cd versions && ls -t')
+    local('cd versions && ls -t | tail -n +{} | xargs -r rm -rf'.format(number))
+    local('cd versions && ls -t')
+
+    # Remote cleanup
     path = '/data/web_static/releases'
-    sudo('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+    print(f"Cleaning remote archives at {path}, keeping {number-1} most recent.")
+    sudo('ls -t {}'.format(path))
+    sudo('cd {} && ls -t | tail -n +{} | xargs -r rm -rf'.format(path, number))
+    sudo('ls -t {}'.format(path))
