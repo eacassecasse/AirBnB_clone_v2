ef30635ad6b2d5f5f19c0374b623631acd951ec5
Edmilson de Azevedo Cassecasse <2015edmilsonb1327@gmail.com>
2024-07-16 02:42:44 -0700

    fix: Running commands using sudo on the remote server because of permissions

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,13 +3,12 @@
 Fabric script to create and distribute an archive to web servers.
 """
 
-import os
+from os.path import exists
 from datetime import datetime
-from fabric.api import env, local, put, run
+from fabric.api import env, local, put, sudo
 
 # Hosts IP and user of the web server web-01 and web-02
 env.hosts = ["54.157.136.194", "100.25.134.41"]
-env.user = "ubuntu"
 
 
 def do_pack():
@@ -55,13 +54,13 @@ def do_deploy(archive_path):
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        sudo('mkdir -p {}{}/'.format(path, no_ext))
+        sudo('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        sudo('rm /tmp/{}'.format(file_n))
+        sudo('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        sudo('rm -rf {}{}/web_static'.format(path, no_ext))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
