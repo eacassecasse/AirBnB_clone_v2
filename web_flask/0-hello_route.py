514a3869ce6a844f251a2e18eabd12e5e5de9581
Edmilson Cassecasse <2015edmilsonb1327@gmail.com>
2024-07-29 09:40:36 +0200

    fix: Correcting PEP8 issues

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,41 @@
 #!/usr/bin/python3
+"""
+A simple Flask Application
+
+This script creates a basic Flask web application that displays
+"Hello HBNB!" when accessing the root URL ("/").
+
+Usage:
+    - Run the script.
+    - Access the root URL ("/") in your web browser to see the message.
+
+Example:
+    $ python3 -m web_flask.0-hello_route
+
+Attributes:
+    app (Flask): The Flask application object.
+"""
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
+    """
+    Root Route Function
+
+    This function defines a route ("/") in a Flask application.
+
+    Attributes:
+        strict_slashes (bool): A flag indicating whether to redirect requests
+        with trailing slashes to the route without the trailing slash.
+        Default is False.
+
+    Returns:
+        str: A greeting message "Hello HBNB!".
+    """
     return "Hello HBNB!"
 
 
